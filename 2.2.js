cd7e81276921b6ad42263fef8b0464a652a5bbbd
marksmanship021 <black.sword021@gmail.com>
2024-02-24 21:05:18 +0200

    остальные задания

---
+++ 2.2.js
@@ -0,0 +1,8 @@
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+let bookHarryPotter = {
+    title: 'Гарри Поттер и философский камень',
+    pageCount: 352,
+    genre: 'fantasy'
+ }
+ 
+ console.log(bookHarryPotter);
---
+++ 2.4.js
@@ -0,0 +1,18 @@
+// - Створити масив з 10 об'єктами які описують сутніть "користувач".
+//  Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+let user14 =  [
+{ name: 'Elena Johnson' },
+{ username: 'elenaj89'  },
+{ password: 'SecurePassword123'  },
+{ age: 32 },
+{ hairColor: 'Brown'  },
+{ gender: 'Female'  },
+{ height: 167  },
+{ email: 'elena.j@example.com'  },
+{ country:  'United States'  },
+{ language: 'English' }
+
+]
+
+console.log(user14);

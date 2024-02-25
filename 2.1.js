951a73e8408c09c1ba248df0ffd428575424e928
marksmanship021 <black.sword021@gmail.com>
2024-02-25 20:30:54 +0200

    Добавлен файл 2.1.js

--- 2.1.js
+++
@@ -1,4 +0,0 @@
-// Масиви та об'єкти:
-// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
-let Messengers = ["WhatsApp", "Viber", "Telegram", "SnapChat", "Discord", "Signal", "Skype", "iMessage", "Dust", "Google-Chat"];
-console.log(Messengers);

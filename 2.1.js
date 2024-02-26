f4d907ef989ee3ab2bca084516a902e47e877e92
marksmanship021 <black.sword021@gmail.com>
2024-02-26 08:45:44 +0200

    редактировал 6 файл

---
+++ 2.1.js
@@ -0,0 +1,4 @@
+// Масиви та об'єкти:
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+let Messengers = ["WhatsApp", "Viber", "Telegram", "SnapChat", "Discord", "Signal", "Skype", "iMessage", "Dust", "Google-Chat"];
+console.log(Messengers);
---
+++ 2.8.js
@@ -0,0 +1,35 @@
+// - Скласти розклад на тиждень за домопоги switch.
+//  Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день
+//  (можна замість плану на день, назву дня англійською).
+
+// alert("- Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).")
+// let result = prompt("enter your day of the week", "sunday")
+// switch (result) {
+//     case "sunday": 
+//     console.log("sunday list")
+//     break;
+
+//     case "monday": 
+//     console.log("monday list")
+//     break;
+
+//     case "tuesday": 
+//     console.log("tuesday list")
+//     break;
+
+//     case "wednesday": 
+//     console.log("wednesday list")
+//     break;
+
+//     case "thursday": 
+//     console.log("thursday list")
+//     break;
+
+//     case "friday": 
+//     console.log("friday list")
+//     break;
+
+//     case "saturday": 
+//     console.log("saturday list")
+//     break;
+// }

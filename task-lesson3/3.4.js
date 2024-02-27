c216357778cfcba75f4700c3cc2fdddf8931b61a
marksmanship021 <black.sword021@gmail.com>
2024-02-27 20:48:09 +0200

    3.6

---
+++ task-lesson3/3.4.js
@@ -0,0 +1,6 @@
+// За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+let Beta = 0;
+while (Beta < 20) {
+    Beta++;
+    document.write(`<div>MozzarellaPizza${Beta}</div>`)
+}
---
+++ task-lesson3/3.5.js
@@ -0,0 +1,7 @@
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+{
+   document.write(`<ul>`)
+   for(let Umbrella of listOfItems)
+   document.write(`<li>${Umbrella}</li>`)
+   document.write(`</ul>`)
+}
